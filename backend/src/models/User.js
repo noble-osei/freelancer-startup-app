7a48d42a7d1f34ef3e2ae4dea893bce233968d4a
noble-osei <boakyenoble9966@gmail.com>
2026-01-12 15:52:25 +0000

    feat: add custom User model methods

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -29,5 +30,14 @@ const userSchema = new mongoose.Schema({
     enum: ["user", "admin"]
   }
 });
+
+userSchema.pre("save", async function() {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password)
+};
 
 export default mongoose.model("User", userSchema)
