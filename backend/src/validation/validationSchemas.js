7165a6aa70a9a3ccb56ad68cb78acecd061ec77d
noble-osei <boakyenoble9966@gmail.com>
2026-01-12 15:31:06 +0000

    fix: configure login and signup schema

--- backend/src/validation/validationSchemas.js
+++ backend/src/validation/validationSchemas.js
@@ -1,11 +1,16 @@
 import Joi from "joi";
 
-export const userBodySchema = Joi.object({
+export const signupSchema = Joi.object({
   name: Joi.string().required().trim(),
   username: Joi.string().required().trim(),
   email: Joi.string().email().required().trim(),
   password: Joi.string().min(8).max(30).required(),
   role: Joi.string().valid("user", "admin")
+});
+
+export const loginSchema = Joi.object({
+  usernameOrEmail: Joi.string().required(),
+  password: Joi.string().min(8).max(30).required()
 });
 
 export const projectBodySchema = Joi.object({
