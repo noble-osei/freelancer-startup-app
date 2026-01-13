951ba4187f2f98f109bd437d52d8328f9acaf959
noble-osei <boakyenoble9966@gmail.com>
2026-01-13 06:07:30 +0000

    feat: handler async functions and add signup control

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,13 @@
+import User from "../models/User.js";
+import asyncHandler from "../middleware/asyncHandler.js";
+
+export const signup = asyncHandler(async(req, res) => {
+  const { name, username, email, password, role } = req.body;
+
+  await User.create({ name, username, email, password, role });
+
+  res.status(201).json({
+    success: true,
+    message: "User has been successfully created"
+  })
+});
