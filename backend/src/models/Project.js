f6834b47a8c5aefb9c4525e7b007b48136df1789
noble-osei <boakyenoble9966@gmail.com>
2026-01-14 06:18:15 +0000

    feat: add timestamp to Project and Task model

--- backend/src/models/Project.js
+++ backend/src/models/Project.js
@@ -9,6 +9,10 @@ const projectSchema = new mongoose.Schema({
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
 });
 
--- backend/src/models/Task.js
+++ backend/src/models/Task.js
@@ -14,6 +14,10 @@ const taskSchema = new mongoose.Schema({
   project: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Project"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
 });
 
