01126d81a48ce34dd30f437b96c94f74e79f370f
noble-osei <boakyenoble9966@gmail.com>
2026-01-21 14:34:32 +0000

    chore: set up Signup and Login routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import SignupPage from './pages/SignupPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
 
 function App() {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/signup' element={<SignupPage />} />
+      <Route path='/login' element={<LoginPage />} />
+    </Routes>
   )
 }
 
