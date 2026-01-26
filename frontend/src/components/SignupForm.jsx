f85507a492a9d75006079e5d8d5dfcc9a035b0c3
noble-osei <boakyenoble9966@gmail.com>
2026-01-26 15:48:34 +0000

    feat: add client Signup Page

--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -1,21 +1,107 @@
 import React from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-function SignupForm() {
-  const [ fullName, setFullName ] = useState("");
-  const [ username, setUsername ] = useState("");
-  const [ email, setEmail ] = useState("");
-  const [ password, setPassword ] = useState("");
-  const [ confirmPassword, setConfirmPassword ] = useState("");
-  const [ error, setError ] = useState(null);
+function SignupForm({ onSubmit }) {
+  const [ formData, setFormData ] = useState({
+    name: "",
+    username: "",
+    email: "",
+    password: ""
+  });
+  const [ showPassword, setShowPassword ] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(formData)
+  };
 
   return (
-    <div className='w-full max-w-md bg-base-100'>
-      <div className="card">
+    <div className='w-full max-w-100'>
+      <div className="card bg-base-100 shadow-xl">
         <div className="card-body">
 
           <div className='text-center'>
-            <h2 className='text-2xl font-semibold'>Login</h2>
+            <h2 className='text-2xl font-semibold'>Signup</h2>
           </div>
+
+          <form className='space-y-4 mt-6' onSubmit={handleSubmit} method='POST'>
+            {/* Full Name input */}
+            <div className="form-control"> 
+              <input 
+                type="text"
+                name='name'
+                className='input input-bordered w-full'
+                placeholder='Full Name'
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            {/* Username input */}
+            <div className="form-control">
+              <input 
+                type="text"
+                name='username'
+                className='input input-bordered w-full'
+                placeholder='Username'
+                value={formData.username}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            {/* Email input */}
+            <div className="form-control">
+              <input 
+                type="email"
+                name='email'
+                className='input input-bordered w-full'
+                placeholder='Email'
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            {/* Password input */}
+            <div className="form-control relative">
+              <input 
+                type={showPassword? "text": "password"}
+                name='password'
+                className='input input-bordered w-full'
+                placeholder='Create Password'
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+
+              <button
+                type='button'
+                className='absolute right-2 top-1/2 -translate-y-1/2 text-sm'
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                { showPassword ? "Hide": "Show"}
+              </button>
+            </div>
+
+            <button type="submit" className='btn btn-primary w-full'>
+              Signup
+            </button>
+
+            <p className='text-center text-base-content/70'>
+              Already have an account?
+              <Link to={"/login"} className='link link-primary ml-1'>Login</Link>
+            </p>
+          </form>
         </div>
       </div>
     </div>
--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,11 +1,28 @@
-import React, { useState } from 'react'
-import SignupForm from '../components/SignupForm.jsx'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+
+import SignupForm from '../components/SignupForm.jsx';
+import api from '../api/axios.js';
 
 function SignupPage() {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (data) => {
+    try {
+      await api.post('/auth/signup', data);
+
+      toast.success('Account created successful!');
+      navigate('/login');
+    } catch (error) {
+      const message = error.response?.data?.message || "Something went wrong. Try again!";
+      toast.error(message);
+    }
+  };
 
   return (
-    <div className='bg-base-200'>
-      <SignupForm />
+    <div className='min-h-screen flex justify-center items-center bg-base-200 relative'>
+      <SignupForm onSubmit={handleSubmit} />
     </div>
   )
 }
