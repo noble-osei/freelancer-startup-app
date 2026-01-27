52fe071393599ce8c16249b86b8708174ac2b4a5
noble-osei <boakyenoble9966@gmail.com>
2026-01-27 17:47:05 +0000

    feat: add interceptors on unauthenticated requests

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,6 +3,31 @@ import axios from 'axios'
 const api = axios.create({
   baseURL: "http://localhost:5002/api",
   withCredentials: true
-})
+});
+
+const refreshApi = axios.create({
+  baseURL: "http://localhost:5002/api",
+  withCredentials: true
+});
+
+api.interceptors.response.use(
+  (response) => response, async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry &&
+      originalRequest.url !== "/auth/refresh") {
+      originalRequest._retry = true;
+
+      try {
+        await refreshApi.post("/auth/refresh");
+        return api(originalRequest);
+      } catch (error) {
+        window.location.href = "/login";
+      }
+    }
+
+    return Promise.reject(error)
+  }
+);
 
 export default api
